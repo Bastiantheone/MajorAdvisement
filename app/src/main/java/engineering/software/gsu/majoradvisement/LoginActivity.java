a1141b04242f6bec0d6e8f5de6615f1bef427221
Bastian Wieck <b.wieck@gmx.de>
2017-04-13 13:51:01 -0400

    start GM

--- app/src/main/java/engineering/software/gsu/majoradvisement/LoginActivity.java
+++ app/src/main/java/engineering/software/gsu/majoradvisement/LoginActivity.java
@@ -1,5 +1,6 @@
 package engineering.software.gsu.majoradvisement;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,16 +8,18 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        activity = this;
         loginButton = (Button)findViewById(R.id.login_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // FIXME start GM and get first question
+                GM.initialize(getBaseContext()).nextQuestion().display(activity);
             }
         });
     }
